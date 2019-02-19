dd61a89adb637d1c78aa0fcd749ffaa965981670
Steven Gutierrez <havoc2787@gmail.com>
2019-02-18 23:44:11 -0500

    Working on it

--- js/script.js
+++ js/script.js
@@ -43,12 +43,68 @@ design.on('click', function() {
     }
 });
 
+
 //http://api.jquery.com/prop/
 activities.on('change', function() {
+    const main = $('input[name="all"]');
+    const jsFrameworks = $('input[name="js-frameworks"]');
+    const express = $('input[name="express"]');
+    const jsLibs = $('input[name="js-libs"]');
+    const node = $('input[name="node"]');
+    let count = 0;
+
+
+    if (this.checked && this.name === "all") {
+        count += 200;
+    } else if (this.checked === false && this.name === "all") {
+        count -= 200;
+    }
+
+
+    //Disables 'Express Workshop' if 'JavaScript Frameworks Workshop' is selected
     if (this.checked && this.name === "js-frameworks") {
-        $('#express').prop("disabled", true);
+        express.prop("disabled", true);
+        express.parent().css( "color", "grey" );
+    } else if (this.checked === false && this.name === "js-frameworks") {
+        express.prop("disabled", false);
+        express.parent().css( "color", "black" );
+    }
+
+
+    //Disables 'JavaScript Frameworks Workshop' if 'Express Workshop' is selected
+    if (this.checked && this.name === "express") {
+        jsFrameworks.prop("disabled", true);
+        jsFrameworks.parent().css( "color", "grey" );
+    } else if (this.checked === false && this.name === "express") {
+        jsFrameworks.prop("disabled", false);
+        jsFrameworks.parent().css( "color", "black" );
+    }
+    
+
+    //Disables 'Node.js Workshop' if 'JavaScript Libraries Workshop' is selected
+    if (this.checked && this.name === "js-libs") {
+        node.prop("disabled", true);
+        node.parent().css( "color", "grey" );
+    } else if (this.checked === false && this.name === "js-libs") {
+        node.prop("disabled", false);
+        node.parent().css( "color", "black" );
+    }
+
+
+    //Disables 'JavaScript Libraries Workshop' if 'Node.js Workshop' is selected
+    if (this.checked && this.name === "node") {
+        jsLibs.prop("disabled", true);
+        jsLibs.parent().css( "color", "grey" );
+    } else if (this.checked === false && this.name === "node") {
+        jsLibs.prop("disabled", false);
+        jsLibs.parent().css( "color", "black" );
+    }
+
+
+    if (count) {
+        total.show();
     } else {
-        $('#express').prop("disabled", false);
+        total.hide();
     }
 });
 
